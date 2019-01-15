0bd51175610c4adb0f67fc99b219e420a4d938f7
zhang zhongyao <17854217027@163.com>
2019-01-15 11:19:21 +0800

    Update IMDB数据集.py

--- IMDB数据集.py
+++ IMDB数据集.py
@@ -1,14 +1,13 @@
 from keras.datasets import imdb
-(train_data, train_label), (test_data, test_label) = imdb.load_data(num_words=1000)
-
-word_index = imdb.get_word_index()
-reverse_word_index = dict(
-    [(value, key) for (key, value) in word_index.items()]
-)
-decoded_review = ' '.join([reverse_word_index.get(i-3, '?') for i in train_data[0]])
-
 import numpy as np
+from keras import models
+from keras import layers
+import matplotlib.pyplot as plt
 
+#读取数据
+(train_data, train_label), (test_data, test_label) = imdb.load_data(num_words=10000)
+
+#向量化—one-hot编码方式
 def vectorize_sequences(sequences, dimension=10000):
     results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
@@ -20,31 +19,26 @@ x_text = vectorize_sequences(test_data)
 y_train = np.asarray(train_label).astype('float32')
 y_test = np.asarray(test_label).astype('float32')
 
-from keras import models
-from keras import layers
-from keras import optimizers
-from keras import losses
-
+# 创建全连接网络模型
 model = models.Sequential()
-model.add(layers.Dense(16, activation='tanh', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='tanh'))
-model.add(layers.Dense(16, activation='tanh'))
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
+# 编译网络
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
+# 划分验证集和训练集
 x_val = x_train[:10000]
 partial_x_train = x_train[10000:]
 
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
 
+# 训练网络
 history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512,validation_data=(x_val, y_val))
-# results = model.evaluate(x_text, y_test)
-# print(results)
 
-import matplotlib.pyplot as plt
-
+# 绘制损失值 
 history_dict = history.history
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
